afa23eb60235d39cd10e8162862e1e037a9853bb
CookieNyanCloud <emil8yunusov@gmail.com>
2022-01-13 00:02:49 +0300

    fix

--- schema/000001_init_schema.up.sql
+++ schema/000001_init_schema.up.sql
@@ -1,5 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE IF NOT EXISTS userbalance
 (
     id      UUID PRIMARY KEY,
@@ -11,8 +9,7 @@ CREATE TABLE IF NOT EXISTS transactions
     user_id     UUID        NOT NULL REFERENCES userbalance (id) ON DELETE CASCADE,
     operation   varchar(16) NOT NULL,
     sum         DECIMAL     NOT NULL,
-    date        timestamp   NOT NULL DEFAULT (now()),
-    description varchar(20)          DEFAULT '',
+    date        timestamp   DEFAULT (now()),
+    description varchar(20) DEFAULT '',
     user_to     UUID REFERENCES userbalance (id)
---     idempotency_key UUID        NOT NULL UNIQUE
 );
